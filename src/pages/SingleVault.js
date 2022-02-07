0d005cc45b0c462b1faa5051fb0dad79833a1fd9
flashfish0x <flashfish0x@protonmail.com>
2022-02-07 16:49:44 +0000

    hide harvest button when no tenderly

--- src/pages/SingleVault.js
+++ src/pages/SingleVault.js
@@ -5,63 +5,67 @@ import { useState, useEffect, useCallback } from "react";
 
 
 
-function SingleVaultPage(singleVault){
-  const {tenderlyProvider, initProvider, closeProvider, defaultProvider, setupTenderly} = useRPCProvider();
+function SingleVaultPage({value}){
+	const {tenderlyProvider, initProvider, closeProvider, defaultProvider, setupTenderly} = useRPCProvider();
+    const [allS, setAlls] = useState([])
+    const [harvestedS, setHarvested] = useState([])
 
-    let [allS, setAlls] = useState([])
-    let [harvestedS, setHarvested] = useState([])
+    console.log("inputting ", value)
 
-    console.log("inputting ", singleVault)
-    useEffect(() => {
-      AllStrats(singleVault, defaultProvider).then(v => { setAlls(v)})
-    }, [singleVault]);
+	//Handle the setAll
+	const	onSetAll = useCallback(async () => {
+		const	_all = await AllStrats(value, defaultProvider);
+		setAlls(_all || []);
+    }, [value, defaultProvider]);
+	useEffect(() => onSetAll(), [onSetAll]);
 
+	//Handle the button to harvest all
     const	onHarvestMultiple = useCallback(async () => {
-      const	_harvested = await HarvestMultiple(allS, singleVault, tenderlyProvider);
-      setHarvested(_harvested, []);
-    }, [tenderlyProvider, allS, singleVault]);
+		const	_harvested = await HarvestMultiple(allS, value, tenderlyProvider);
+		setHarvested(_harvested || []);
+    }, [tenderlyProvider, allS, value]);
 
-    const showApr = (strat) =>{
-      if(!strat.succeded){
-        return
-      }
+    const showApr = (strat) => {
+		if(!strat.succeded){
+			return
+		}
 
-      let profit = strat.paramsAfter.totalGain - strat.beforeGain
+		let profit = strat.paramsAfter.totalGain - strat.beforeGain
         let loss = strat.paramsAfter.totalLoss - strat.beforeLoss
         let percent = 0
-        if (strat.beforeDebt > 0){
-          if (loss > profit){
-              percent = -1 * loss / strat.beforeDebt 
-          }else{
-            percent = profit / strat.beforeDebt
-          }
-        }
-        let over_year = (100*percent * 8760 / strat.lastTime).toLocaleString(undefined, {maximumFractionDigits:2})
-            
-      return " APR " + over_year + "% " 
+        if (strat.beforeDebt > 0) {
+			if (loss > profit){
+				percent = -1 * loss / strat.beforeDebt 
+			} else {
+				percent = profit / strat.beforeDebt
+			}
+		}
+		let over_year = (100*percent * 8760 / strat.lastTime).toLocaleString(undefined, {maximumFractionDigits:2})    
+      	return " APR " + over_year + "% ";
     }
-    if(allS.length ==0){
-        return(
-          <div>loading strats...</div>
-      )
-      }
-      console.log(allS)
-      console.log(harvestedS)
 
-      const listItems = allS.map((strat) =>
-      <div key={strat.address}> 
-      <br />
-        <div>Strat: {strat.name} - {strat.address}</div>
-        <div>Lastharvest: {strat.lastTime.toLocaleString(undefined, {maximumFractionDigits:2})}h - Real ratio: {(100*strat.beforeDebt/strat.vaultAssets).toLocaleString(undefined, {maximumFractionDigits:2})}% - Desired ratio: {(strat.debtRatio/100).toLocaleString(undefined, {maximumFractionDigits:2})}% </div>
-        <div> {harvestedS.length > 0 ? (strat.succeded ? showApr(strat) : "Failed Harvest ")  : ""} <a target="_blank" href={strat.tenderlyURL}>{harvestedS.length > 0 && "Tenderly Link"} </a></div>
+    if(allS.length == 0) {
+        return(<div>loading strats...</div>)
+    }
+    
+	console.log(allS)
+    console.log(harvestedS)
+
+    const listItems = allS.map((strat) => (
+		<div key={strat.address}> 
+			<br />
+			<div>Strat: {strat.name} - {strat.address}</div>
+    		<div>Lastharvest: {strat.lastTime.toLocaleString(undefined, {maximumFractionDigits:2})}h - Real ratio: {(100*strat.beforeDebt/strat.vaultAssets).toLocaleString(undefined, {maximumFractionDigits:2})}% - Desired ratio: {(strat.debtRatio/100).toLocaleString(undefined, {maximumFractionDigits:2})}% </div>
+			<div>{harvestedS.length > 0 ? (strat.succeded ? showApr(strat) : "Failed Harvest ")  : ""} <a target="_blank" href={strat.tenderlyURL}>{harvestedS.length > 0 && "Tenderly Link"} </a></div>
       </div>
-  );
+	));
 
-  return(
-    <div><button  onClick={onHarvestMultiple}> Harvest All?</button>
-        
-        {listItems}</div>
-)
+	return(
+		<div>
+			<button disabled={!tenderlyProvider} onClick={onHarvestMultiple}> Harvest All?</button>
+			{listItems}
+		</div>
+	);
 }
 
 
