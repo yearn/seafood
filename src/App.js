4c035be58e4d39503eff0af0a234ee9d2147dd03
flashfish0x <flashfish0x@protonmail.com>
2022-02-10 13:57:19 +0000

    irst

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Route, Routes, BrowserRouter} from 'react-router-dom';
 import SingleVaultPage from './pages/SingleVault';
+import MasterchefPage from './pages/Masterchef';
 import DefaultPage from './pages/Default';
 import {RPCProviderContextApp} from './context/useRpcProvider';
 
@@ -12,6 +13,7 @@ function App() {
 					<Routes>
 						<Route path={'/'} exact={true} element={<DefaultPage />}></Route>
 						<Route path={'/vault'} element={<SingleVaultPage />}></Route>
+            <Route path={'/masterchef'} element={<MasterchefPage />}></Route>
 					</Routes>
 				</BrowserRouter>
 			</div>
