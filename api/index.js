17e1cd7eba1359785a9e575b494bb970b58fe9cc
flashfish0x <flashfish0x@protonmail.com>
2022-02-17 09:14:09 +0000

    testing backend

--- api/index.js
+++ api/index.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
         
-app.get('/', (req, res) => res.send('Home Page Route'));
+app.get('/api/', (req, res) => res.send('Home Page Route'));
 
-app.get('/about', (req, res) => res.send('About Page Route'));
+app.get('/api/about', (req, res) => res.send('About Page Route'));
 
-app.get('/portfolio', (req, res) => res.send('Portfolio Page Route'));
+app.get('/api/portfolio', (req, res) => res.send('Portfolio Page Route'));
 
-app.get('/contact', (req, res) => res.send('Contact Page Route'));
+app.get('/api/testAPI', (req, res) => res.send('Contact Page Route'));
 
 const port = process.env.PORT || 9000;
 
