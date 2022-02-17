0f887fe474418caeda9d163645acac99972b27e6
flashfish0x <flashfish0x@protonmail.com>
2022-02-17 09:04:58 +0000

    test indexs

---
+++ api/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+        
+app.get('/', (req, res) => res.send('Home Page Route'));
+
+app.get('/about', (req, res) => res.send('About Page Route'));
+
+app.get('/portfolio', (req, res) => res.send('Portfolio Page Route'));
+
+app.get('/contact', (req, res) => res.send('Contact Page Route'));
+
+const port = process.env.PORT || 9000;
+
+app.listen(port, () => console.log(`Server running on ${port}, http://localhost:${port}`));
