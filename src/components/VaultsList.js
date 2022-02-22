29b6740dfad8b3060938d4affea32b8a6cec624b
flashfish0x <flashfish0x@protonmail.com>
2022-02-22 12:00:42 +0000

    add clickable icons

--- src/components/VaultsList.js
+++ src/components/VaultsList.js
@@ -1,5 +1,7 @@
 import React, {useState,useEffect} from 'react';
 import axios from '../axios';
+import {BsBoxArrowInUpRight, BsClipboardPlus} from 'react-icons/bs';
+import {GetExplorerLink} from '../utils/utils';
 
 
 
@@ -31,7 +33,7 @@ function VaultButtons({provider, clickFunction}){
 				if(filterCurve && (vault.name.includes('urve') || vault.name.includes('crv'))){
 					return '';
 				}else{
-					return <div key={vault.address}><button  onClick={() => clickFunction(vault)}> {vault.name}{' - '}{vault.version}{' - '}{vault.address}</button></div>;
+					return <div key={vault.address}><button  onClick={() => clickFunction(vault)}> {vault.name}{' - '}{vault.version}{' - '}{vault.address}</button>< BsClipboardPlus onClick={() => navigator.clipboard.writeText(vault.address)} /><a href={GetExplorerLink(provider, vault.address)}>< BsBoxArrowInUpRight   /></a></div>;
 				}
 			})}</div>
 	</div>
