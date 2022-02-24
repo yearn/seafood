d9fb87f51e662707d9fcd1110e26102000766b40
murderteeth <murdertxxth@gmail.com>
2022-02-23 14:00:38 -1000

    Fix anchor tag default style

--- src/components/VaultsList.js
+++ src/components/VaultsList.js
@@ -48,6 +48,7 @@ function VaultButtons({provider, clickFunction}){
 						{TruncateAddress(vault.address)}
 						<BsClipboardPlus title={`Copy ${vault.address} to your clipboard`} onClick={() => navigator.clipboard.writeText(vault.address)} />
 						<a href={GetExplorerLink(provider, vault.address)} title={`Explore ${vault.address}`}><BsBoxArrowInUpRight /></a>
+						<a href={'https://google.com'}>{'Google!'}</a>
 					</div>
 				</div>;
 			})}
