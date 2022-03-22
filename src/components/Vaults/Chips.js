b762177f8648da1282a0d183009105380b37b875
murderteeth <murdertxxth@gmail.com>
2022-03-21 23:41:48 -1000

    Filter vaults by favorites

--- src/components/Vaults/Chips.js
+++ src/components/Vaults/Chips.js
@@ -1,13 +1,30 @@
 import React from 'react';
+import {BsStar, BsStarFill} from 'react-icons/bs';
 import {useFilter} from './useFilter';
 
 export default function Chips() {
 	const {chips, setChips} = useFilter();
+
+	function toggle(chip) {
+		return () => {
+			console.log('toggle', chip);
+			setChips(chips => {
+				return {...chips, [chip]: !chips[chip]};
+			});
+		};
+	}
+
 	return <div className={'flex flex-col items-center'}>
 		<div className={'flex flex-row'}>
+			<div onClick={toggle('favorites')}
+				className={`chip iconic favorite ${chips.favorites ? 'hot' : ''}`}>
+				{!chips.favorites && <BsStar />}
+				{chips.favorites && <BsStarFill />}
+			</div>
+
 			{chips.tags.map(tag => {
-				return <div key={tag} onClick={() => {setChips({...chips, [tag]: !chips[tag]});}} 
-					className={`chip-${tag} chip ${chips[tag] ? ` hot-${tag}` : ''}`}>{tag}</div>;
+				return <div key={tag} onClick={toggle(tag)} 
+					className={`chip-${tag} chip ${chips[tag] ? `hot-${tag}` : ''}`}>{tag}</div>;
 			})}
 		</div>
 	</div>;
