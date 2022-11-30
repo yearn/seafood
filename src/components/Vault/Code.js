d505b683614ae77b9034a8e9b528141fe1f51885
murderteeth <murdertxxth@gmail.com>
2022-11-30 09:44:43 +0000

    Adapt strategist-ms pr flow

--- src/components/Vault/Code.js
+++ src/components/Vault/Code.js
@@ -1,15 +1,17 @@
-import React, {useCallback, useMemo, useState} from 'react';
-import {TbCopy, TbCheck, TbGitPullRequest} from 'react-icons/tb';
-import useScrollOverpass from '../../context/useScrollOverpass';
+import React, {useCallback, useEffect, useMemo, useState} from 'react';
+import {TbCopy, TbCheck} from 'react-icons/tb';
+import {BiGitBranch, BiGitPullRequest} from 'react-icons/bi';
 import {Button, Input, TextArea} from '../controls';
 import {useAuth} from '../../context/useAuth';
-import * as gh from '../../utils/github';
+import {GithubClient} from '../../utils/github';
 import {useDebouncedCallback} from 'use-debounce';
+import dayjs from 'dayjs';
+import config from '../../config.json';
 
 export default function Code({vault, debtRatioUpdates}) {
 	const [copied, setCopied] = useState(false);
-	const {showClassName} = useScrollOverpass();
-	const {bearer} = useAuth();
+	const {bearer, profile} = useAuth();
+	const gh = useMemo(() => new GithubClient(bearer), [bearer]);
 
 	const updates = useMemo(() => {
 		const result = [];
@@ -29,10 +31,10 @@ export default function Code({vault, debtRatioUpdates}) {
 	}, [vault, debtRatioUpdates]);
 
 	const linesOfCode = useMemo(() => {
-		const lines = ['@sign'];
+		const lines = ['', '@sign'];
 		if(updates.length) {
 			updates.sort((a, b) => a.delta > b.delta ? 1 : -1);
-			lines.push('def auto_debt_adjust():');
+			lines.push('def go_seafood():');
 			lines.push('\tstrategies=[]');
 			lines.push('');
 			lines.push(`\t# ${vault.name}`);
@@ -48,7 +50,6 @@ export default function Code({vault, debtRatioUpdates}) {
 			lines.push('');
 			lines.push('\tharvest_n_check_many(safe, strategies)');
 			lines.push('');
-			lines.push('\n');
 		}
 		return lines;
 	}, [vault, updates]);
@@ -67,13 +68,12 @@ export default function Code({vault, debtRatioUpdates}) {
 	const defaultCommitMessage = useMemo(() => {
 		const body = [];
 		updates.forEach(update => {
-			body.push(update.address);
 			body.push(update.name);
-			body.push(`Change debt ratio by ${update.delta > 0 ? '+' : ''}${update.delta} bps`);
+			body.push(`- change debt ratio by ${update.delta > 0 ? '+' : ''}${update.delta} bps`);
 			body.push('');
 		});
 		return {
-			headline: `${vault?.name} Debt Ratio Updates`,
+			headline: `feat: Change Debt Ratios, ${vault?.name}`,
 			body: body.join('\n')
 		};
 	}, [vault, updates]);
@@ -84,27 +84,48 @@ export default function Code({vault, debtRatioUpdates}) {
 	const debounceBody = useDebouncedCallback(value => {setBody(value);}, 250);
 	const commitMessage = useMemo(() => ({headline, body}), [headline, body]);
 
+	const nextBranchName = useCallback(async () => {
+		const today = dayjs(new Date()).format('MMM-DD');
+		const prefix = `refs/heads/seafood/${profile.name}/${today}/`;
+		const refs = await gh.getRefs(config.sms.owner, config.sms.repo, prefix);
+		const nonce = Math.max(0, ...refs.map(ref => parseInt(ref.name))) + 1;
+		return `seafood/${profile.name}/${today}/${nonce}`;
+	}, [gh, profile]);
+
+	const [defaultBranchName, setDefaultBranchName] = useState(`${config.sms.repo}/refs/heads/seafood/`);
+	useEffect(() => {
+		nextBranchName().then(branch => 
+			setDefaultBranchName(`${config.sms.repo}/refs/heads/${branch}`));
+	}, [nextBranchName]);
+
+	const smsScriptExpression = `${config.sms.main}:${config.sms.script}`;
+	const [smsMain, setSmsMain] = useState();
+	useEffect(() => {
+		gh.getObjectText(config.sms.owner, config.sms.repo, smsScriptExpression).then(
+			main => setSmsMain(main.split('\n')));
+	}, [gh, smsScriptExpression]);
+
 	const onPr = useCallback(async () => {
-		const fake = Math.floor(Math.random() * 1_000_000).toString();
-		const owner = 'murderteeth';
-		const repo = 'fishtank';
-		const branchName = fake;
-		const path = `vault-migrations/${fake}.py`;
-		const main = await gh.getRef(bearer, owner, repo, 'refs/heads/main');
-		const branch = await gh.createRef(bearer, main, branchName);
-		await gh.createCommitOnBranch(bearer, branch, commitMessage, {
+		const main = await gh.getRef(config.sms.owner, config.sms.repo, `refs/heads/${config.sms.main}`);
+		const branch = await gh.createRef(main, await nextBranchName());
+		const newSmsMain = `${smsMain.join('\n')}${linesOfCode.join('\n')}\n`;
+		await gh.createCommitOnBranch(branch, commitMessage, {
 			additions: [{
-				path,
-				contents: window.btoa(linesOfCode.join('\n'))
+				path: config.sms.script,
+				contents: window.btoa(unescape(encodeURIComponent(newSmsMain)))
 			}]
 		});
 		const compareUrl = gh.makeCompareUrl(branch);
 		window.open(compareUrl, '_blank', 'noreferrer');
-	}, [bearer, linesOfCode, commitMessage]);
+	}, [linesOfCode, smsMain, commitMessage, gh, nextBranchName]);
 
 	return <div className={'relative w-full h-full'}>
-		<div className={'max-h-full px-4 sm:px-8 pt-12 pb-24 flex flex-col overflow-x-auto'}>
-			<div className={'py-4 flex flex-col gap-4'}>
+		<div className={'max-h-full px-4 sm:px-8 pt-12 pb-20 flex flex-col overflow-x-auto'}>
+			<div className={'flex items-center text-xs gap-1 text-primary-600'}>
+				<BiGitBranch />
+				<div>{defaultBranchName}</div>
+			</div>
+			<div className={'pt-2 flex flex-col gap-4'}>
 				<Input
 					placeholder={'Pull Request Title'}
 					defaultValue={defaultCommitMessage.headline}
@@ -126,14 +147,34 @@ export default function Code({vault, debtRatioUpdates}) {
 					bg-gray-300 dark:bg-gray-800
 					focus:ring-0 focus:border-primary-400 focus:bg-gray-200
 					focus:dark:border-selected-600
-					rounded-md shadow-inner`} />
+					rounded-md shadow-inner resize-none`} />
 			</div>
+			<div className={'py-2 text-xs text-primary-600'}>{config.sms.script}</div>
 			<div className={`
 				py-4 border border-gray-300 dark:border-gray-800
 				overflow-x-scroll rounded-md`}>
+				{(smsMain || ['', '', '', '']).slice(-4, -1).map((line, index) => 
+					<div key={index} className={'flex items-center'}>
+						<div className={`
+							ml-2 mr-4
+							font-mono text-right opacity-30 dark:text-secondary-400
+							whitespace-nowrap`}>
+							{smsMain?.length + index + 1 - 4}&nbsp;&nbsp;
+						</div>
+						<div className={'whitespace-nowrap opacity-40'}>
+							{Array.from(line).filter(c => c === '\t').map((_, index) => <span key={index}>&emsp;</span>)}
+							{line.replace('\t', '')}
+						</div>
+					</div>
+				)}
 				{linesOfCode.map((line, index) => 
 					<div key={index} className={'flex items-center'}>
-						<div className={'ml-2 mr-4 w-8 min-w-[2rem] text-right dark:text-secondary-400/60'}>{''}{index + 1}</div>
+						<div className={`
+							ml-2 mr-4
+							font-mono text-right dark:text-secondary-400/60
+							whitespace-nowrap`}>
+							{smsMain?.length + index + 1 - 1}{' + '}
+						</div>
 						<div className={'whitespace-nowrap'}>
 							{Array.from(line).filter(c => c === '\t').map((_, index) => <span key={index}>&emsp;</span>)}
 							{line.replace('\t', '')}
@@ -144,13 +185,12 @@ export default function Code({vault, debtRatioUpdates}) {
 		</div>
 
 		<div className={`
-			absolute bottom-0 w-full px-4 py-4
+			absolute bottom-0 w-full px-4 sm:px-8 py-4
 			flex items-center justify-end gap-4
 			border-t border-white dark:border-secondary-900
-			rounded-b-lg
-			${showClassName}`}>
+			rounded-b-lg`}>
 			<Button disabled={updates?.length === 0} icon={copied ? TbCheck : TbCopy} onClick={onCopy} className={'w-48'} />
-			<Button disabled={updates?.length === 0 || !bearer} icon={TbGitPullRequest} onClick={onPr} className={'w-48'} />
+			<Button disabled={updates?.length === 0 || !bearer} icon={BiGitPullRequest} onClick={onPr} className={'w-48'} />
 		</div>
 	</div>;
 }
