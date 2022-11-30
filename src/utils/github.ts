770bd966056ca551fde2a8503537d2ce77f22ca3
murderteeth <murdertxxth@gmail.com>
2022-11-30 09:44:43 +0000

    Adapt strategist-ms pr flow

--- src/utils/github.ts
+++ src/utils/github.ts
@@ -39,145 +39,206 @@ export interface PullRequest {
 	url: string
 }
 
-export function makeCompareUrl(branch: Ref) {
-	return `https://github.com/${branch.repository.owner.login}/${branch.repository.name}/compare/${branch.name}?expand=1`;
-}
+export class GithubClient {
+	bearer: string;
+	constructor(bearer: string) {
+		this.bearer = bearer;
+	}
 
-export async function getRef(
-	bearer: string, 
-	owner: string, 
-	repo: string, 
-	path: string
-) : Promise<Ref> {
-	const gql = `
-	query ref($owner: String!, $repo: String!, $path: String!) {
-		repository(owner: $owner, name: $repo) {
-			ref(qualifiedName: $path) {
-				id
-				name,
-				target {
-					oid
-				},
-				repository {
-					id,
+	private async octoql<T>(gql: string, parameters: object) : Promise<T> {
+		return await octoql<T>(gql, {
+			...parameters,
+			headers: {
+				authorization: `token ${this.bearer}`
+			}
+		});
+	}
+
+	makeCompareUrl(branch: Ref) {
+		return `https://github.com/${branch.repository.owner.login}/${branch.repository.name}/compare/${branch.name}?expand=1`;
+	}
+	
+	async getRef(
+		owner: string,
+		repo: string, 
+		path: string
+	) : Promise<Ref> {
+		const gql = `
+		query ref($owner: String!, $repo: String!, $path: String!) {
+			repository(owner: $owner, name: $repo) {
+				ref(qualifiedName: $path) {
+					id
 					name,
-					owner {
-						login
+					target {
+						oid
+					},
+					repository {
+						id,
+						name,
+						owner {
+							login
+						}
 					}
 				}
 			}
-		}
-	}`;
+		}`;
+	
+		const {repository} = await this.octoql<{repository: {ref: Ref}}>(gql, {
+			owner, repo, path
+		});
+	
+		return repository.ref;
+	}
 
-	const {repository} = await octoql<{repository: {ref: Ref}}>(gql, {
-		owner, repo, path,
-		headers: {
-			authorization: `token ${bearer}`
-		}
-	});
-
-	return repository.ref;
-}
-
-export async function createRef(
-	bearer: string, 
-	base: Ref, 
-	name: string
-) : Promise<Ref> {
-	const gql = `
-	mutation($input: CreateRefInput!) {
-		createRef(input: $input) {
-			ref {
-				id
-				name,
-				target {
-					oid
-				},
-				repository {
-					id,
-					name,
-					owner {
-						login
+	async getRefs(
+		owner: string,
+		repo: string, 
+		prefix: string
+	) : Promise<Ref[]> {
+		const gql = `
+		query ref($owner: String!, $repo: String!, $prefix: String!) {
+			repository(owner: $owner, name: $repo) {
+				refs(refPrefix: $prefix, first: 100) {
+					edges {
+						node {
+							id,
+							name,
+							target {
+								oid
+							},
+							repository {
+								id,
+								name,
+								owner {
+									login
+								}
+							}
+						}
 					}
 				}
 			}
-		}
-	}`;
+		}`;
+	
+		const {repository} = await this.octoql<{repository: {
+			refs: {edges: {node: Ref}[]}
+		}}>(gql, {
+			owner, repo, prefix
+		});
+	
+		return repository.refs.edges.map(e => e.node);
+	}
 
-	const result = await octoql<{createRef: {ref: Ref}}>(gql, {
-		input: {
-			name: `refs/heads/${name}`,
-			oid: base.target.oid,
-			repositoryId: base.repository.id
-		},
-		headers: {
-			authorization: `token ${bearer}`
-		}
-	});
-
-	return result.createRef.ref;
-}
-
-export async function createCommitOnBranch(
-	bearer: string,
-	branch: Ref,
-	message: Message,
-	fileChanges: FileChanges
-) : Promise<Commit> {
-	const gql = `
-	mutation($input: CreateCommitOnBranchInput!) {
-		createCommitOnBranch(input: $input) {
-			commit {
-				oid
+	async getObjectText(
+		owner: string, 
+		repo: string, 
+		objectExpression: string
+	) : Promise<string> {
+		const gql = `
+		query ref($owner: String!, $repo: String!, $objectExpression: String!) {
+			repository(owner: $owner, name: $repo) {
+				content: object(expression: $objectExpression) {
+					... on Blob{
+						text
+					}
+				}
 			}
-		}
-	}`;
+		}`;
 
-	const result = await octoql<{createCommitOnBranch: {commit: Commit}}>(gql, {
-		input: {
-			branch: {
-				id: branch.id
-			},
-			expectedHeadOid: branch.target.oid,
-			fileChanges,
-			message
-		},
-		headers: {
-			authorization: `token ${bearer}`
-		}
-	});
-
-	return result.createCommitOnBranch.commit;
-}
-
-export async function createPullRequest(
-	bearer: string,
-	base: Ref,
-	head: Ref,
-	message: Message
-) : Promise<PullRequest> {
-	const gql = `
-	mutation($input: CreatePullRequestInput!) {
-		createPullRequest(input: $input) {
-			pullRequest {
-				id,
-				url
+		const {repository} = await this.octoql<{repository: {content: {text: string}}}>(gql, {
+			owner, repo, objectExpression
+		});
+	
+		return repository.content.text;
+	}
+	
+	async createRef(
+		base: Ref, 
+		name: string
+	) : Promise<Ref> {
+		const gql = `
+		mutation($input: CreateRefInput!) {
+			createRef(input: $input) {
+				ref {
+					id
+					name,
+					target {
+						oid
+					},
+					repository {
+						id,
+						name,
+						owner {
+							login
+						}
+					}
+				}
 			}
-		}
-	}`;
-
-	const result = await octoql<{createPullRequest: {pullRequest: PullRequest}}>(gql, {
-		input: {
-			baseRefName: `refs/heads/${base.name}`,
-			headRefName: `refs/heads/${head.name}`,
-			repositoryId: base.repository.id,
-			title: message.headline,
-			body: message.body
-		},
-		headers: {
-			authorization: `token ${bearer}`
-		}
-	});
-
-	return result.createPullRequest.pullRequest;
+		}`;
+	
+		const result = await this.octoql<{createRef: {ref: Ref}}>(gql, {
+			input: {
+				name: `refs/heads/${name}`,
+				oid: base.target.oid,
+				repositoryId: base.repository.id
+			}
+		});
+	
+		return result.createRef.ref;
+	}
+	
+	async createCommitOnBranch(
+		branch: Ref,
+		message: Message,
+		fileChanges: FileChanges
+	) : Promise<Commit> {
+		const gql = `
+		mutation($input: CreateCommitOnBranchInput!) {
+			createCommitOnBranch(input: $input) {
+				commit {
+					oid
+				}
+			}
+		}`;
+	
+		const result = await this.octoql<{createCommitOnBranch: {commit: Commit}}>(gql, {
+			input: {
+				branch: {
+					id: branch.id
+				},
+				expectedHeadOid: branch.target.oid,
+				fileChanges,
+				message
+			}
+		});
+	
+		return result.createCommitOnBranch.commit;
+	}
+	
+	async createPullRequest(
+		base: Ref,
+		head: Ref,
+		message: Message
+	) : Promise<PullRequest> {
+		const gql = `
+		mutation($input: CreatePullRequestInput!) {
+			createPullRequest(input: $input) {
+				pullRequest {
+					id,
+					url
+				}
+			}
+		}`;
+	
+		const result = await this.octoql<{createPullRequest: {pullRequest: PullRequest}}>(gql, {
+			input: {
+				baseRefName: `refs/heads/${base.name}`,
+				headRefName: `refs/heads/${head.name}`,
+				repositoryId: base.repository.id,
+				title: message.headline,
+				body: message.body
+			}
+		});
+	
+		return result.createPullRequest.pullRequest;
+	}
 }
