99ebcd7287919ae65f5961686a0f8248fe8089a4
murderteeth <murdertxxth@gmail.com>
2022-12-02 05:13:12 +0000

    Summon the service worker

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import './chains.css';
 import App from './App';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 ReactDOM.render(
 	<React.StrictMode><App /></React.StrictMode>, document.getElementById('root')
 );
+
+// serviceWorkerRegistration.unregister();
+serviceWorkerRegistration.register();
