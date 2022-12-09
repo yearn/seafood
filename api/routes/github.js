94de77f4c81bbca6f355ea6e362bca276f396c49
murderteeth <murdertxxth@gmail.com>
2022-12-09 05:44:25 +0000

    Use GitHub App for authorization instead of Github OAuth app

--- api/routes/github.js
+++ api/routes/github.js
@@ -2,9 +2,9 @@ const axios = require('axios');
 const express = require('express');
 const router = express.Router();
 
-router.post('/callback', async function(req, res, next) {
+router.post('/callback', async function(req, res) {
 	try {
-		const result = await axios({
+		const response = await axios({
 			method: 'post',
 			headers: {accept: 'application/json'},
 			url: 'https://github.com/login/oauth/access_token'
@@ -12,17 +12,38 @@ router.post('/callback', async function(req, res, next) {
 				+ `&client_secret=${process.env.GITHUB_CLIENT_SECRET}`
 				+ `&code=${req.body.code}`
 		});
-
-		if(result.data.error) {
-			console.log('error', response.data.error);
-			res.status(500).send('An error occurred creating a GitHub access token.');
+		if(response.data.error) {
+			console.log('response.data.error', response.data.error);
+			res.status(500).send('An error occurred');
 		} else {
-			res.status(200).send({bearer: result.data.access_token});
+			res.status(200).send({...response.data});
 		}
-
 	} catch(error) {
-    console.log('error', error);
-    res.status(500).send('An error occurred creating a GitHub access token.');
+		console.log(error);
+		res.status(500).send('An error occurred');
+	}
+});
+
+router.post('/refreshToken', async function(req, res) {
+	try {
+		const response = await axios({
+			method: 'post',
+			headers: {accept: 'application/json'},
+			url: 'https://github.com/login/oauth/access_token'
+				+ `?client_id=${process.env.GITHUB_CLIENT_ID}`
+				+ `&client_secret=${process.env.GITHUB_CLIENT_SECRET}`
+				+ '&grant_type=refresh_token'
+				+ `&refresh_token=${req.body.refresh_token}`
+		});
+		if(response.data.error) {
+			console.log('response.data.error', response.data.error);
+			res.status(500).send('An error occurred');
+		} else {
+			res.status(200).send({...response.data});
+		}
+	} catch(error) {
+		console.log(error);
+		res.status(500).send('An error occurred');
 	}
 });
 
