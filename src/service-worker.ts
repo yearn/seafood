f4cab0f497afa184bfad7c945ee04ae940588cce
murderteeth <murdertxxth@gmail.com>
2022-12-27 23:19:44 +0000

    Fix @typescript-eslint/no-unused-vars

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -13,7 +13,6 @@ import {ExpirationPlugin} from 'workbox-expiration';
 import {precacheAndRoute, createHandlerBoundToURL} from 'workbox-precaching';
 import {registerRoute} from 'workbox-routing';
 import {StaleWhileRevalidate} from 'workbox-strategies';
-import config from './config.json';
 
 declare const self: ServiceWorkerGlobalScope;
 
