6c4909c01a86a1a653a4c4a31ecb4567e50d2949
murderteeth <murdertxxth@gmail.com>
2023-02-03 01:46:22 +0000

    Instrument with Sentry

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import {createRoot} from 'react-dom/client';
+import * as Sentry from '@sentry/react';
+import {BrowserTracing} from '@sentry/tracing';
 import './index.css';
 import './chains.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
+Sentry.init({
+	dsn: process.env.REACT_APP_SENTRY_DSN,
+	integrations: [new BrowserTracing()],
+	tracesSampleRate: parseFloat(process.env.REACT_APP_SENTRY_TRACE_SAMPLE_RATE || '0.0'),
+});
+
 const container = document.getElementById('root');
 const root = createRoot(container!); // eslint-disable-line @typescript-eslint/no-non-null-assertion
-root.render(<React.StrictMode><App /></React.StrictMode>);
+root.render(<React.StrictMode>
+	<Sentry.ErrorBoundary>
+		<App />
+	</Sentry.ErrorBoundary>
+</React.StrictMode>);
 
 serviceWorkerRegistration.register();
