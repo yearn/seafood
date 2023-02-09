315375c2a5c687ec647cda63bfaa3e739d225bbe
murderteeth <murdertxxth@gmail.com>
2023-02-09 00:46:04 +0000

    Scrub sentry client tags from logging
    Sentry includes tags with basic device info like
    connection speed and memory.

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,13 @@
 import React from 'react';
 import {createRoot} from 'react-dom/client';
 import * as Sentry from '@sentry/react';
-import {BrowserTracing} from '@sentry/tracing';
+import {setupSentry} from './utils/sentry';
 import './index.css';
 import './chains.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
-Sentry.init({
-	dsn: process.env.REACT_APP_SENTRY_DSN,
-	integrations: [new BrowserTracing()],
-	tracesSampleRate: parseFloat(process.env.REACT_APP_SENTRY_TRACE_SAMPLE_RATE || '0.0'),
-});
+setupSentry();
 
 const container = document.getElementById('root');
 const root = createRoot(container!); // eslint-disable-line @typescript-eslint/no-non-null-assertion
