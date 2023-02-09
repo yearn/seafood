89ef204b438f0783d9eebde32f852e9d81d9b3e4
murderteeth <murdertxxth@gmail.com>
2023-02-09 22:53:35 +0000

    Disable sentry tracing
    This way we only use sentry for error logging.

--- src/utils/sentry.ts
+++ src/utils/sentry.ts
@@ -1,5 +1,4 @@
 import * as Sentry from '@sentry/react';
-import {BrowserTracing} from '@sentry/tracing';
 
 function scrubTags(value: Sentry.Event) {
 	if(Object.keys(value).some(k => k === 'tags')) delete value.tags;
@@ -9,24 +8,22 @@ function isObject(value: unknown) {
 	return value && typeof value === 'object' && value.constructor === Object;
 }
 
-function scrubSensativeData(value: Sentry.Event) {
+function scrubSensitiveData(value: Sentry.Event) {
 	scrubTags(value);
 	for(const property of Object.values(value)) {
-		if(isObject(property)) scrubSensativeData(property);
+		if(isObject(property)) scrubSensitiveData(property);
 	}
 }
 
 export function setupSentry() {
 	Sentry.init({
 		dsn: process.env.REACT_APP_SENTRY_DSN,
-		integrations: [new BrowserTracing()],
-		tracesSampleRate: parseFloat(process.env.REACT_APP_SENTRY_TRACE_SAMPLE_RATE || '0.0'),
 		beforeSend: event => {
-			scrubSensativeData(event);
+			scrubSensitiveData(event);
 			return event;
 		},
 		beforeSendTransaction: event => {
-			scrubSensativeData(event);
+			scrubSensitiveData(event);
 			return event;
 		}
 	});
