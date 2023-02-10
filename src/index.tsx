7728aacd71a33520881a15df43d63a962ca294bd
murderteeth <89237203+murderteeth@users.noreply.github.com>
2023-02-09 19:03:08 -0500

    Upgrade to React 18 (#145)

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {createRoot} from 'react-dom/client';
 import './index.css';
 import './chains.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
-ReactDOM.render(
-	<React.StrictMode><App /></React.StrictMode>, 
-	document.getElementById('root')
-);
+const container = document.getElementById('root');
+const root = createRoot(container!); // eslint-disable-line @typescript-eslint/no-non-null-assertion
+root.render(<React.StrictMode><App /></React.StrictMode>);
 
 serviceWorkerRegistration.register();
