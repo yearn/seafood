c27990c7e77af4b0ef0e11905fd43946e89b8246
murderteeth <murdertxxth@gmail.com>
2023-05-04 07:32:33 +0000

    Match notification height to new filter height

--- src/components/Notification.tsx
+++ src/components/Notification.tsx
@@ -2,7 +2,7 @@ import React, {ReactNode} from 'react';
 
 export default function Notification({className, children}: {className?: string, children: ReactNode}) {
 	return <div className={`
-	fixed z-50 top-0 w-full h-32 sm:h-14 px-4 sm:px-0
+	fixed z-50 top-0 w-full h-32 sm:h-16 px-4 sm:px-0
 	flex flex-col sm:flex-row items-center justify-center gap-4
 	text-lg bg-secondary-900 bg-stripes bg-stripes-black
 	${className}`}>
